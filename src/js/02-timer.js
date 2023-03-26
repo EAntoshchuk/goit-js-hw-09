d34385865a4f7cc1bb8a7c3e90f69878f0d5b305
Eduard Antoshchuk <e.antoshchuk@gmail.com>
2023-03-26 23:34:04 +0300

    hw9_task2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -3,60 +3,84 @@ import 'flatpickr/dist/flatpickr.min.css';
 
 const refs = {
   startBtn: document.querySelector('button[data-start]'),
-  stopBtn: document.querySelector('button[data-action-stop]'),
-  clockFace: document.querySelector('.js-clockface'),
+  datetimePicker: document.querySelector('#datetime-picker'),
+  daysEl: document.querySelector('[data-days]'),
+  hoursEl: document.querySelector('[data-hours]'),
+  minsEl: document.querySelector('[data-minutes]'),
+  secsEl: document.querySelector('[data-seconds]'),
 };
-const timer = {
-  intervalId: null,
-  isAcvtive: false,
-  start() {
-    if (this.isAcvtive) {
-      return;
-    }
 
-    const startTime = Date.now();
-
-    this.intervalId = setInterval(() => {
-      const currentTime = Date.now();
-      const deltaTime = currentTime - startTime;
-      const timeComponents = getTimeComponents(deltaTime);
-      updateClockFace(timeComponents);
-      //   console.log(currentTime - startTime);
-      console.log(`${hours}:${mins}:${secs}`);
-      console.log(
-        `${pad(new Date(deltaTime).getUTCHours())}:${pad(
-          new Date(deltaTime).getMinutes()
-        )}:${pad(new Date(deltaTime).getSeconds())}`
-      );
-    }, 1000);
-  },
-  stop() {
-    clearInterval(this.intervalId);
-    this.isAcvtive = false;
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
   },
 };
+
+flatpickr(refs.datetimePicker, options);
 
 refs.startBtn.addEventListener('click', () => {
-  timer.start();
+  const selectedDate = refs.datetimePicker.value;
+  const currentDate = new Date();
+  const endDate = new Date(selectedDate);
+  if (endDate < currentDate) {
+    window.alert('Please choose a date in the future');
+    return;
+  }
+  const deltaDate = endDate - currentDate;
+  const timeTillDate = convertMs(deltaDate);
+  refs.daysEl.textContent = addLeadingZero(timeTillDate.days);
+  refs.hoursEl.textContent = addLeadingZero(timeTillDate.hours);
+  refs.minsEl.textContent = addLeadingZero(timeTillDate.minutes);
+  refs.secsEl.textContent = addLeadingZero(timeTillDate.seconds);
+  const intervalId = setInterval(() => {
+    disableBtn();
+    const timeLeft = endDate - new Date();
+    const updatedTimeTillDate = convertMs(timeLeft);
+    refs.daysEl.textContent = addLeadingZero(updatedTimeTillDate.days);
+    refs.hoursEl.textContent = addLeadingZero(updatedTimeTillDate.hours);
+    refs.minsEl.textContent = addLeadingZero(updatedTimeTillDate.minutes);
+    refs.secsEl.textContent = addLeadingZero(updatedTimeTillDate.seconds);
+    if (timeLeft < 1) {
+      clearInterval(intervalId);
+    }
+  }, 1000);
 });
 
-refs.stopBtn.addEventListener('click', () => {
-  timer.stop();
-});
-
-function updateClockFace({ hours, mins, secs }) {
-  refs.clockFace.textContent = `${hours}:${mins}:${secs}`;
-}
-
-function pad(value) {
+function addLeadingZero(value) {
   return String(value).padStart(2, '0');
 }
 
-function getTimeComponents(time) {
-  const hours = pad(
-    Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
-  );
-  const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-  const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
-  return { hours, mins, secs };
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  return { days, hours, minutes, seconds };
 }
+
+function disableBtn() {
+  refs.startBtn.disabled = true;
+}
+
+// function getTimeComponents(time) {
+//   const hours = pad(
+//     Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+//   );
+//   const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+//   const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+//   return { hours, mins, secs };
+// }
