cd14877b6b29da93fab63bd6b86a1ff0d3682b53
Eduard Antoshchuk <e.antoshchuk@gmail.com>
2023-03-29 01:11:17 +0300

    hw9_finished

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,103 +1,40 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-    resolve('promise successfully commited');
-  } else {
-    // Reject
-    reject('promise failed to commit');
-  }
-}
+import Notiflix from 'notiflix';
 
 const refs = {
-  startBtn: document.querySelector('.js-startBtn'),
-  winnerField: document.querySelector('js-winner'),
-  progressField: document.querySelector('.js-progress'),
-  tableBody: document.querySelector('.js-results-table > tbody'),
+  createPromiseBtn: document.querySelector('button[type="submit"]'),
+  delayEl: document.querySelector('input[name="delay"]'),
+  stepEl: document.querySelector('input[name="step"]'),
+  amountEl: document.querySelector('input[name="amount"]'),
 };
 
-refs.startBtn.addEventListener('click', onStart);
+refs.createPromiseBtn.addEventListener('click', onClick);
 
-let raceCounter = 0;
-
-function onStart() {
-  raceCounter += 1;
-  const promises = horses.map(run);
-
-  updateWinnerField('');
-  updateProgressField('заїзд почався');
-  determineWinner(promises);
-  waitForAll(promises);
-}
-
-function determineWinner(horsesP) {
-  Promise.race(horsesP).then(({ horse, time }) => {
-    updateWinnerField(`Переміг ${horse}, прийшовши за ${time}`);
-    updateTableResults({ horse, time, raceCounter });
-  });
-}
-
-function waitForAll(horsesP) {
-  Promise.all(horsesP).then(() => {
-    updateProgressField(`заїзд завершено`);
-  });
-}
-
-function updateWinnerField(message) {
-  refs.winnerField.textContent = message;
-}
-
-function updateProgressField(message) {
-  refs.progressField.textContent = message;
-}
-
-// function updateTableResults({ horse, time, raceCounter }) {
-//   const tr = (
-//     <tr>
-//       <td>8</td>
-//     </tr>
-//   );
-//   refs.tableBody.insertAdjacentHTML('beforeend', tr);
-// }
-
-function run(horse) {
+function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
-    const time = getRandomTime(2000, 3500);
+    const shouldResolve = Math.random() > 0.3;
     setTimeout(() => {
-      resolve({ horse, time });
-    }, time);
+      if (shouldResolve) {
+        // Fulfill
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      } else {
+        // Reject
+        reject(`❌ Rejected promise ${position} in ${delay}ms`);
+      }
+    }, delay);
   });
 }
 
-Promise.all(promises).then(() => {
-  console.log('promises');
-});
+function onClick(e) {
+  e.preventDefault();
+  delay = Number(refs.delayEl.value);
+  step = Number(refs.stepEl.value);
+  amount = Number(refs.amountEl.value);
+  console.log(delay);
 
-run('Mango').then(x => console.log(x));
-
-const makeOrder = dish => {
-  const DELAY = 1000;
-
-  return new createPromise((resolve, reject) => {
-    const passed = Math.random() > 0.5;
-
-    setTimeout(() => {
-      if (passed) {
-        resolve('ват ваш заказ:${dish}');
-      }
-      reject('нет продуктов');
-    }, DELAY);
-  });
-};
-//   }
-//     .then(onFulfilled)
-//     .then(x => {
-//       console.log(x);
-//     })
-//     .then(y => {
-//       console.log(y);
-//     })
-//     .catch(error => console.log(error))
-//     .finally(() => console.log('committed anyway'));
-
-//  }
+  for (let i = 1; i <= amount; i += 1) {
+    createPromise(i, delay)
+      .then(resolve => Notiflix.Notify.success(resolve))
+      .catch(reject => Notiflix.Notify.failure(reject));
+    delay += step;
+  }
+}
